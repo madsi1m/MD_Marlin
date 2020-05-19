f6cea9a300d2666e50e7d2b883d5af7a90b40956
Michael D'Silva <michael.dsilva@outlook.com>
2020-05-19 21:55:39 +1000

    fine tune esteps for fake BMG extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -742,7 +742,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 410 } //MD { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 405.94 } //MD { 80, 80, 400, 93 }
 
 /**
  * Default Max Feed Rate (mm/s)
